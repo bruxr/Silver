341f5bb872397300e157df974da3327684fb4cc5
brux <imoz39@gmail.com>
2015-04-05 18:35:58 +0800

    add sm scrapers

---
+++ app/Scrapers/SmLanang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * SM Lanang Scraper
+ *
+ * @package Silver
+ * @author brux
+ * @since 0.1.0
+ */
+
+namespace App\Scrapers;
+
+class SmLanang extends SmMalls
+{
+  
+  const SLUG = 'smlanang';
+  
+  const BRANCH_CODE = 'SMLA';
+
+}
--- app/Scrapers/SmMalls.php
+++ app/Scrapers/SmMalls.php
@@ -15,6 +15,8 @@ namespace App\Scrapers;
 use App\Exceptions\ParseException;
 use App\Services\Log;
 
+use Carbon\Carbon;
+
 class SmMalls extends Base
 {
 
@@ -27,13 +29,156 @@ class SmMalls extends Base
   
   public function fetch()
   {
-    $this->fetchMovies(static::BRANCH_CODE);
+    $movies = $this->fetchMovies();
+    $this->processMovies($movies);
     return $this;
   }
   
-  protected function fetchMovies($branch_code)
+  protected function query($method, $args = [])
+  {
+    $defaults = [
+      'method' => $method,
+      'branch_code' => static::BRANCH_CODE
+    ];
+    $args = array_merge($defaults, $args);
+    
+    $context = $this->createContext($args);
+    $resp = file_get_contents(self::URL, false, $context);
+    
+    $resp = @json_decode($resp, true);
+    if ( $resp === null )
+    {
+      throw new ParseException('SM', 'Failed to parse response. ('. json_last_error() .')', $args);
+    }
+    return $resp;
+    
+  }
+  
+  protected function createContext($data)
+  {
+    $data = http_build_query($data);
+    $headers = [
+      'Accept: application/json',
+      'User-Agent: Silverbot',
+      'X-Contact: brux <brux.romuar@gmail.com>'
+    ];
+    
+    return stream_context_create([
+      'http' => [
+        'method' => 'POST',
+        'header' => implode("\r\n", $headers),
+        'content' => $data
+      ]
+    ]);
+  }
+  
+  protected function fetchMovies()
+  {
+    return $this->query('listMovies');
+  }
+  
+  protected function processMovies($movies)
+  {
+    foreach ( $movies as $movie )
+    {
+      $this->processMovie($movie);
+    }
+  }
+  
+  protected function processMovie($movie)
   {
     
+    $resp = $this->query('listScreeningTime', ['movie_name' => $movie['movie_name']]);
+    
+    // Process the title
+    $movie = $this->cleanMovieTitle(trim($movie['movie_name']));
+    $movie = strtolower($movie);
+    if ( ! isset($this->movies[$movie]) )
+    {
+      $this->movies[$movie] = [
+        'title' => $movie,
+        'screening_times' => []
+      ];
+    }
+    
+    // Start processing each record
+    foreach ( $resp as $cinema => $items )
+    {
+      foreach ( $items as $r )
+      {
+      
+        $cinema = $this->processCinema($r['CinemaCode']);
+        $format = $this->extractFormat($r['FilmFormat']);
+        $price = $this->processPrice($movie, $r['Price']);
+        $time = $this->processTime($r['StartTime']);
+      
+        $arr = [
+          'cinema' => $cinema,
+          'time' => $time,
+          'ticket' => [
+            'price' => $price
+          ]
+        ];
+      
+        if ( $format == '2D' && $cinema == 'IMAX' )
+        {
+          $arr['format'] = 'IMAX';
+        }
+        elseif ( $format != '2D' )
+        {
+          $arr['format'] = $format;
+        }
+      
+        $this->movies[$movie]['screening_times'][] = $arr;
+      }
+    }
+    
+  }
+  
+  protected function processCinema($cinema)
+  {
+    $cinema = trim($cinema);
+    if ( ! preg_match('/^([0-9]|IMAX)$/', $cinema) )
+    {
+      throw new ParseException('SM', 'Invalid cinema.', $cinema);
+    }
+    elseif ( $cinema != 'IMAX' )
+    {
+      $cinema = (int) $cinema;
+    }
+    return $cinema;
+  }
+  
+  protected function extractFormat($format)
+  {
+    $format = trim($format);
+    if ( $format == 'F2D' || $format == 'STANDARD' )
+    {
+      return '2D';
+    }
+    elseif ( $format == 'F3D' )
+    {
+      return '3D';
+    }
+    else
+    {
+      throw new ParseException('SM', 'Unknown film format.', $format);
+    }
+  }
+  
+  protected function processPrice($movie, $price)
+  {
+    $int_price = (int) $price;
+    if ( $int_price == 0 )
+    {
+      throw new ParseException('SM', 'Failed to convert price to int for the movie '. $movie, $price);
+    }
+    return $int_price;
+  }
+
+  protected function processTime($time)
+  {
+    return Carbon::parse($time);
   }
 
 }
