a6b3124b0625f7f2eaee488fff4caaa9cb76d509
brux <imoz39@gmail.com>
2015-04-11 14:53:24 +0800

    remove keys class and use dotenv instead

--- app/start.php
+++ app/start.php
@@ -3,14 +3,13 @@
 use Slim\Slim;
 use App\Middleware\Environment;
 use App\Middleware\Services;
-use App\Services\Keys;
 use App\Services\Log;
 
 $app = new Slim(array(
   'log.writer'          => Log::instance(),
   'cookies.secure'      => true,
   'cookies.httponly'    => true,
-  'cookies.secret_key'  => Keys::get('cookies_secret')
+  'cookies.secret_key'  => getenv('COOKIE_SECRET')
 ));
 
 $app->add(new Environment());
