658a7d875b7d37a8517a412aa1bcecfa25d26afb
brux <imoz39@gmail.com>
2015-04-17 12:05:07 +0800

    add proper callbacks

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,11 +1,31 @@
 <?php
-class TestCase extends PHPUnit_Framework_TestCase
+abstract class TestCase extends PHPUnit_Framework_TestCase
 {
   
   function __construct()
   {
     parent::__construct();
     $this->app = Slim\Slim::getInstance();
+  }
+  
+  public function setUp()
+  {
+    $this->before();
+  }
+  
+  public function before()
+  {
+    
+  }
+  
+  public function tearDown()
+  {
+    $this->after();
+  }
+  
+  public function after()
+  {
+    
   }
   
 }
