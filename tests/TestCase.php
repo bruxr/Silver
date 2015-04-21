07a29f39371e9b9db7db1d63039bf590b27d4bbc
brux <imoz39@gmail.com>
2015-04-21 09:07:15 +0800

    use test bootstrap

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,39 +1,32 @@
 <?php
+
 abstract class TestCase extends PHPUnit_Framework_TestCase
 {
   
-  function __construct()
-  {
-    parent::__construct();
-    $this->app = Slim\Slim::getInstance();
-    $this->setupDb();
-  }
-  
-  public function setUp()
-  {
-    $this->before();
-  }
-  
-  public function before()
-  {
-    
-  }
-  
-  public function tearDown()
-  {
-    $this->after();
-  }
-  
-  public function after()
-  {
-    
-  }
-  
-  private function setupDb()
-  {
-    $this->app->container->singleton('db_driver', function() {
+    function __construct()
+    {
+        parent::__construct();
+        $this->app = Slim\Slim::getInstance();
+    }
+
+    public function setUp()
+    {
+        $this->before();
+    }
+
+    public function before()
+    {
+
+    }
+
+    public function tearDown()
+    {
+        $this->after();
+    }
+
+    public function after()
+    {
+
+    }
       
-    });
-  }
-  
 }
---
+++ tests/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once 'app/Core/bootstrap/bootstrap.php';
