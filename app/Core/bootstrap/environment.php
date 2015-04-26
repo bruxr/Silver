e30d6b6b52b8cb8c4de042b1083f1f87820fb984
brux <imoz39@gmail.com>
2015-04-26 17:19:33 +0800

    update logging

--- app/Core/bootstrap/environment.php
+++ app/Core/bootstrap/environment.php
@@ -2,3 +2,12 @@
 
 date_default_timezone_set('Asia/Manila');
 Dotenv::load(ROOT);
+
+if ( $_SERVER['SERVER_NAME'] == 'localhost' )
+{
+    define('SILVER_MODE', 'dev');
+}
+else
+{
+    define('SILVER_MODE', 'live');
+}
