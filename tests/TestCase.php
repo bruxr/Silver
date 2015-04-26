fa811f1aacc1b4177de87cea7256ddf4e405391c
brux <imoz39@gmail.com>
2015-04-26 19:46:27 +0800

    slim 3 doesn't support getInstance()

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,8 @@ abstract class TestCase extends PHPUnit_Framework_TestCase
     function __construct()
     {
         parent::__construct();
-        $this->app = Slim\Slim::getInstance();
+        global $app;
+        $this->app = $app;
     }
 
     public function setUp()
