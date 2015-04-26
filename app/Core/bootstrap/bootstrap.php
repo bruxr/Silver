09b867c8f6a29ddd9128a679c60e48c9a86788c8
brux <imoz39@gmail.com>
2015-04-26 19:46:16 +0800

    only do http stuff when not in cli mode

--- app/Core/bootstrap/bootstrap.php
+++ app/Core/bootstrap/bootstrap.php
@@ -14,4 +14,8 @@ $app = new \Slim\App();
 require_once CORE . '/bootstrap/environment.php';
 require_once CORE . '/bootstrap/services.php';
 
-$app->run();
+// Run the app if we aren't in CLI mode.
+if ( php_sapi_name() !== 'cli' )
+{
+    $app->run();
+}
--- app/Core/bootstrap/environment.php
+++ app/Core/bootstrap/environment.php
@@ -3,11 +3,14 @@
 date_default_timezone_set('Asia/Manila');
 Dotenv::load(ROOT);
 
-if ( $_SERVER['SERVER_NAME'] == 'localhost' )
+if ( php_sapi_name() !== 'cli' )
 {
-    define('SILVER_MODE', 'dev');
-}
-else
-{
-    define('SILVER_MODE', 'live');
+    if ( $_SERVER['SERVER_NAME'] == 'localhost' )
+    {
+        define('SILVER_MODE', 'dev');
+    }
+    else
+    {
+        define('SILVER_MODE', 'live');
+    }
 }
