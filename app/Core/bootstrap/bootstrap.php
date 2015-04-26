9d68b3fa1a07292e8ac346b719d7822204284bab
brux <imoz39@gmail.com>
2015-04-26 20:17:33 +0800

    add test response to root url

--- app/Core/bootstrap/bootstrap.php
+++ app/Core/bootstrap/bootstrap.php
@@ -14,6 +14,10 @@ $app = new \Slim\App();
 require_once CORE . '/bootstrap/environment.php';
 require_once CORE . '/bootstrap/services.php';
 
+$app->get('/', function() {
+    echo 'hello world.';
+});
+
 // Run the app if we aren't in CLI mode.
 if ( php_sapi_name() !== 'cli' )
 {
