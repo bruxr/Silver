29e9badd9357963c748a1d907d52579ad8559f36
brux <imoz39@gmail.com>
2015-04-30 19:11:23 +0800

    add jobs route

---
+++ app/Routes/jobs.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Jobs Route
+ * 
+ * This route is responsible for dispatching jobs sent through
+ * the jobs endpoint.
+ * 
+ * @author Brux
+ * @since  0.1.0
+ */
+$app->get('/jobs/{command:[a-z\-]+}', function($req, $resp, $args) {
+    $command_class = ucwords(mb_ereg_replace('-', ' ', $args['command']));
+    $command_class = mb_ereg_replace(' ', '', $command_class);
+    $command_class = 'App\Commands\\' . $command_class;
+    if ( isset($this[$command_class]) )
+    {
+        $command = new $command_class($req->post());
+        $this['bus']->handle($command);
+    }
+    else
+    {
+        return $this['notFoundHandler']($req, $resp);
+    }
+});
